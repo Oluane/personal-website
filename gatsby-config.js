7fb843b6fd9667ba7c0b408850cd00dbc371f368
Oluane <rochepauline6@gmail.com>
2021-09-28 14:18:27 +0200

    tsconfig paths + tailwind setup with reset css

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "personal-website",
   },
-  plugins: [`gatsby-plugin-postcss`],
+  plugins: [
+    `gatsby-plugin-postcss`, 
+    {
+      resolve: `gatsby-plugin-tsconfig-paths`,
+      options: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+      },
+    }
+  ],
 };
