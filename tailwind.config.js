8b10c611aea7e3e199e5c2dc6a5ff82d27ad49f3
Oluane <rochepauline6@gmail.com>
2021-09-29 17:05:25 +0200

    adding some global style rules + extending TW theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        eigengrau: "hsl(240, 14%, 10%)",
+        "lighter-eigengrau": "#2c2c3a",
+      },
+      backgroundImage: {
+        "gradient-radial-ellipse-b":
+          "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
+      },
+    },
   },
   variants: {
     extend: {},
