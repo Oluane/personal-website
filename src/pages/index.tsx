74dee0d1dee0726bb1b00d214da9096f0bd8c0a0
Oluane <rochepauline6@gmail.com>
2021-10-08 19:41:10 +0200

    WIP page to deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import "@src/styles/global.css";
 import "@fontsource/megrim";
+import "@fontsource/lato";
 
 import React from "react";
 
 import { Hero } from "@src/components/hero/hero";
-import { Navbar } from "@src/components/navbar/navbar";
-import { SecondPart } from "@src/components/second-part";
+// import { Navbar } from "@src/components/navbar/navbar";
+// import { SecondPart } from "@src/components/second-part";
 
 const IndexPage: React.FC = () => {
   return (
     <main>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
-      <SecondPart />
+      {/* <SecondPart /> */}
     </main>
   );
 };
