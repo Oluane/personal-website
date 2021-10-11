65c70fbdaa71776cc017f22b880ed5addb2e4cb5
Oluane <rochepauline6@gmail.com>
2021-10-11 15:57:41 +0200

    cleaning readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,15 @@ import "@fontsource/lato";
 import React from "react";
 
 import { Hero } from "@src/components/hero/hero";
-// import { Navbar } from "@src/components/navbar/navbar";
-// import { SecondPart } from "@src/components/second-part";
+import { Navbar } from "@src/components/navbar/navbar";
+import { SecondPart } from "@src/components/second-part";
 
 const IndexPage: React.FC = () => {
   return (
     <main>
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
-      {/* <SecondPart /> */}
+      <SecondPart />
     </main>
   );
 };
